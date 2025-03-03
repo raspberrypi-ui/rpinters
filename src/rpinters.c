eacd2c2ec67d7e23e105febc33df239a39b1a1f0
Simon Long <simon@raspberrypi.com>
2025-03-03 13:21:19 +0000

    Move gettext override function to parent

--- src/rpinters.c
+++ src/rpinters.c
@@ -112,13 +112,6 @@ void free_plugin (void)
 {
 }
 
-const char *dgetfixt (const char *domain, const char *msgctxid)
-{
-    const char *text = dgettext (domain, msgctxid);
-    if (!strchr (text, 0x04)) return text;
-    return dgettext (domain, strchr (msgctxid, 0x04) + 1);
-}
-
 #else
 
 /*----------------------------------------------------------------------------*/
